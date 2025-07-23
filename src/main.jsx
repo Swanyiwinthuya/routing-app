09a0108e229f72b75573a5929433f0f92c2cac4c
Swanyi <khinkhinbala@Swans-MacBook-Pro.local>
2025-07-23 19:32:34 +0700

    Adding Name Form

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import Root from "./routes/root";
+import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
+import { action as destroyAction } from "./routes/destroy";
 import ErrorPage from "./error-page";
+import Index from "./routes/index";
+import Contact, {
+  loader as contactLoader,
+   action as contactAction,
+} from "./routes/contact";
+import EditContact , {
+  action as editAction,
+} from "./routes/edit";
 
 import {
   createBrowserRouter,
@@ -17,6 +26,28 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+  children: [
+      { index: true, element: <Index /> },
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+        action: contactAction,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      },
+    ],
   },
 ]);
 
